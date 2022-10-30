72b4640c97595b9e47f6aa66df0b78931d1ec388
Мстислав Грацкий <mgratsky@gmail.com>
2022-10-30 23:58:57 +0400

    project

---
+++ yatube/posts/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField("Название", max_length=200)
+    slug = models.SlugField("Уникальное имя (slug)", unique=True)
+    description = models.TextField("Описание")
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField("")
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="posts",
+    )
---
+++ yatube/posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+RECENT_POSTS = 10
+
+
+def index(request):
+    posts = Post.objects.order_by("-pub_date")[:RECENT_POSTS]
+    context = {
+        "posts": posts,
+    }
+    return render(request, "posts/index.html", context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:RECENT_POSTS]
+    context = {
+        "group": group,
+        "posts": posts,
+    }
+    return render(request, "posts/group_list.html", context)
+
+
+# Create your views here.
